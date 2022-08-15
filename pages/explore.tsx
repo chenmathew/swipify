a124f812fde47a6f13958ccf12dfa423c58ea1bf
Mathew Chen <mathewchenpnw@gmail.com>
2022-08-15 02:00:40 -0700

    deleted some files

--- pages/explore.tsx
+++ pages/explore.tsx
@@ -6,27 +6,6 @@ import ReactAudioPlayer from "react-audio-player";
 import Songinfo from "../components/songinfo";
 import Nav from "../components/nav";
 
-/*
-Finished:
-  Getting random track, getting like and storing
-
-TO DO:
-  IMPORTANT!
-  - Add IDs to track object
-    - This is so if there is a dupe in liked and current track, ignore, and can be used to set key
-  - Make a post request to create a playlist https://developer.spotify.com/console/post-playlists/
-    - Then add the tracks using the liked.uri https://developer.spotify.com/console/post-playlist-tracks/
-  - After the user sends to playlist, reset state
-  - Implement a UI
-  - Break down components so it's not messy
-  - Fixed autoplay
-
-Future:
-  - Make background color change depending on album cover
-  - Implement refresh until successful request
-  - Explore mode - No login required
-  - Login mode - login required
-*/
 
 const Home: NextPage = () => {
   const clientId = process.env.NEXT_PUBLIC_CLIENT_ID;
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,27 @@ import Image from "next/image";
 import ReactAudioPlayer from "react-audio-player";
 import Link from "next/link";
 
+/*
+Finished:
+  Getting random track, getting like and storing
+
+TO DO:
+  IMPORTANT!
+  - Add IDs to track object
+    - This is so if there is a dupe in liked and current track, ignore, and can be used to set key
+  - Make a post request to create a playlist https://developer.spotify.com/console/post-playlists/
+    - Then add the tracks using the liked.uri https://developer.spotify.com/console/post-playlist-tracks/
+  - After the user sends to playlist, reset state
+  - Implement a UI
+  - Break down components so it's not messy
+  - Fixed autoplay
+
+Future:
+  - Make background color change depending on album cover
+  - Implement refresh until successful request
+  - Explore mode - No login required
+  - Login mode - login required
+*/
 const Home: NextPage = () => {
   return (
     <div>
